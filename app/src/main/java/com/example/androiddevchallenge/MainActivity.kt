b9602c188b3463b756aa08a18958e621e7b8432f
Shimokawa Takahiro <takahiro.1828@gmail.com>
2021-03-02 21:43:59 +0900

    Implement the navigation

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,12 +18,35 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
+import com.example.androiddevchallenge.data.Puppy
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import dev.chrisbanes.accompanist.coil.CoilImage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,8 +62,203 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "puppyList") {
+        composable("puppyList") {
+            PuppyList(navController = navController, puppyList = createInitialData())
+        }
+        composable(
+            "puppyDetail/{index}",
+            arguments = listOf(navArgument("index") { type = NavType.IntType })
+        ) {
+            PuppyDetail(navController, it.arguments?.getInt("index"))
+        }
+    }
+}
+
+@Composable
+fun PuppyList(navController: NavController, puppyList: List<Puppy>, modifier: Modifier = Modifier) {
+    val scrollState = rememberLazyListState()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Lost puppies") }
+            )
+        }
+    ) {
+        Surface(color = MaterialTheme.colors.background) {
+            LazyColumn(state = scrollState, modifier = modifier) {
+                itemsIndexed(puppyList) { index, puppy ->
+                    PuppyCard(puppy) {
+                        navController.navigate("puppyDetail/$index")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PuppyCard(puppy: Puppy, modifier: Modifier = Modifier, onClick: () -> Unit) {
+    Row(
+        modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = onClick)
+            .padding(16.dp)) {
+        Image(
+            painter = painterResource(id = puppy.imageResId),
+            contentDescription = "puppy",
+            modifier = Modifier
+                .size(78.dp)
+                .clip(RoundedCornerShape(28.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .wrapContentHeight(align = Alignment.CenterVertically)
+        ) {
+            Text(puppy.name, fontWeight = FontWeight.Bold, fontSize = 18.sp)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(puppy.title.orEmpty(), style = MaterialTheme.typography.body2)
+                Text(puppy.message.orEmpty(), style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Composable
+fun PuppyDetail(navController: NavController, index: Int?) {
+    val puppy = getPuppyDetail(index ?: 0)
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Lost puppies") },
+                navigationIcon = { IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(Icons.Rounded.ArrowBack, contentDescription = "back")
+                }}
+            )
+        }
+    ) {
+        Surface(color = MaterialTheme.colors.background) {
+            Column(modifier = Modifier
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState())
+            ) {
+                Image(
+                    painter = painterResource(id = puppy.imageResId),
+                    contentDescription = "puppy",
+                    modifier = Modifier.padding(bottom = 16.dp).clip(RoundedCornerShape(8.dp))
+                )
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Text(text = puppy.name, style = MaterialTheme.typography.body1)
+                    Text(text = puppy.message.orEmpty(), style = MaterialTheme.typography.body1)
+                }
+            }
+        }
+    }
+}
+
+private fun createInitialData(): List<Puppy> {
+    return MutableList(4) { index ->
+        var name: String? = null
+        var imageResId: Int? = null
+        var title: String? = null
+        var message: String? = null
+
+        when (index) {
+            0 -> {
+                name = "Taro"
+                imageResId = R.drawable.puppy1
+                title = "Japan"
+                message = "Age 1"
+            }
+            1 -> {
+                name = "Max"
+                imageResId = R.drawable.puppy2
+                title = "America"
+                message = "Age 1"
+            }
+            2 -> {
+                name = "Rocky"
+                imageResId = R.drawable.puppy3
+                title = "Germany"
+                message = "Age 2"
+            }
+            3 -> {
+                name = "Sam"
+                imageResId = R.drawable.puppy4
+                title = "Australia"
+                message = "Age 1"
+            }
+            else -> {
+                // nothing to do
+            }
+        }
+        Puppy(
+            name.orEmpty(),
+            imageResId ?: 0,
+            title,
+            message
+        )
+    }
+}
+
+// FIXME: create the detail data
+private fun getPuppyDetail(index: Int): Puppy {
+    var name: String? = null
+    var imageResId: Int? = null
+    var title: String? = null
+    var message: String? = null
+
+    when (index) {
+        0 -> {
+            name = "Taro"
+            imageResId = R.drawable.puppy1
+            title = "Japan"
+            message = "A really very cute puppy. Please let me know if you find it."
+        }
+        1 -> {
+            name = "Max"
+            imageResId = R.drawable.puppy2
+            title = "America"
+            message = "A self-paced puppy. Maybe I'm sleeping on the side of the road."
+        }
+        2 -> {
+            name = "Rocky"
+            imageResId = R.drawable.puppy3
+            title = "Germany"
+            message = "A very smart puppy, but often lost."
+        }
+        3 -> {
+            name = "Sam"
+            imageResId = R.drawable.puppy4
+            title = "Australia"
+            message = "Being a serious puppy, he may be looking for a home."
+        }
+        else -> {
+            // nothing to do
+        }
+    }
+
+    return Puppy(
+        name.orEmpty(),
+        imageResId ?: 0,
+        title,
+        message
+    )
+}
+
+@Preview
+@Composable
+fun PuppyCardPreview() {
+    MyTheme {
+//        PuppyCard() {}
     }
 }
 
---
+++ app/src/main/java/com/example/androiddevchallenge/data/Puppy.kt
@@ -0,0 +1,8 @@
+package com.example.androiddevchallenge.data
+
+data class Puppy(
+    val name: String,
+    val imageResId: Int,
+    val title: String? = null,
+    val message: String? = null
+)
